0ba8124a67ab0541332d2ff7df3b4d9066dd9ebb
rubendediego90 <rubendediego90@gmail.com>
2022-10-12 11:24:02 +0200

    itinerario

--- src/components/Itinerario.vue
+++ src/components/Itinerario.vue
@@ -6,10 +6,43 @@
 <template>
   <Title title="Itinerario"/>
     <div class="col-12 column" style="padding:1rem">
-      1.Ceremonia
-      2.Coctel
-      3.Comida
-      4.Baile
-      5.Recena
-    </div>
+  <div class="q-px-lg q-pb-md">
+    <q-timeline color="secondary">
+      <q-timeline-entry
+        title="Ceremonia"
+        subtitle="13:30H"
+        icon="church"
+        body="Un ofreciante dedicará unas palabras. Además hablaran amigos y familiares de los novios."
+        color="pink"
+      />
+      <q-timeline-entry
+        title="Coctel"
+        subtitle="14:30H"
+        icon="tapas"
+        body="Se servirá un magnífico coctel de bienvenida, en el cual habrá comida y bebida."
+      />
+      <q-timeline-entry
+        title="Comida"
+        subtitle="16:00"
+        icon="restaurant"
+        color="green"
+        body="Momento de disfrutar del menú, cuanta con dos platos y postre."
+      />
+      <q-timeline-entry
+        title="Fiesta"
+        subtitle="18:00"
+        icon="nightlife"
+        body="Será hora de bailar y brindar para celebrar el matrimonio."
+        color="red"
+      />
+      <q-timeline-entry
+        title="Recena"
+        subtitle="21:00"
+        color="orange"
+        icon="lunch_dining"
+        body="Una hora antes de finalizar la fiesta habrá una pequeña sorpresa para cenar."
+      />
+    </q-timeline>
+  </div>
+  </div>
 </template>
