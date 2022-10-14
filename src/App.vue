01d2ff8de644aba1c885656fb4859ba692c3be99
rubendediego90 <rubendediego90@gmail.com>
2022-10-14 08:08:41 +0200

    build app

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!--<script setup>
 import ButtonDesplegable from './components/ButtonDesplegable.vue'
 import img from "./assets/manos.jpg";
 import Asistencia from "./components/Asistencia.vue"
@@ -69,4 +69,59 @@ onMounted(async ()=>{
     </q-footer>
 
   </q-layout>
+</template>-->
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-input color="purple-12" v-model="text" label="Label">
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-input>
+
+      <q-input color="teal" filled v-model="text" label="Label">
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-input>
+
+      <q-input color="grey-3" label-color="orange" outlined v-model="text" label="Label">
+        <template v-slot:append>
+          <q-icon name="event" color="orange" />
+        </template>
+      </q-input>
+
+      <q-input color="lime-11" bg-color="green" filled v-model="text" label="Label">
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-input>
+
+      <q-input color="teal" outlined v-model="text" label="Label">
+        <template v-slot:append>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
+          </q-avatar>
+        </template>
+      </q-input>
+
+      <q-input color="orange" standout bottom-slots v-model="text" label="Label" counter clearable>
+        <template v-slot:prepend>
+          <q-icon name="place" />
+        </template>
+        <template v-slot:append>
+          <q-icon name="favorite" />
+        </template>
+
+        <template v-slot:hint>
+          Field hint
+        </template>
+      </q-input>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const text = ref('')
+</script>
