d8530742bbb1dd24eb32ed717da786609cbb63eb
rubendediego90 <rubendediego90@gmail.com>
2022-12-29 10:02:38 +0100

    boton lugar cambiado

--- src/components/Lugar.vue
+++ src/components/Lugar.vue
@@ -7,15 +7,14 @@
 <template>
   <Title title="Lugar"/>
     <div class="col-12 column" style="padding:1rem">
-      <p class="text-justify">Os esperamos en <strong><u>La Vega del Henares</u></strong> donde celebraremos nuestro matrimonio junto a nuestros seres queridos.</p>
+      <span class="text-justify">Os esperamos en <strong><u>La Vega del Henares</u></strong> donde celebraremos nuestro matrimonio junto a nuestros seres queridos.</span>
     </div>
-      <Mapbox/>
-      <div style="display: flex;justify-content: center;align-items: center;margin-top: 1rem;">
+    <div style="display: flex;justify-content: center;align-items: center;">
         <a href="http://maps.google.com/?q=Carretera Madrid Barcelona, 0 Km 33, N-II, 28801 Alcalá de Henares, Madrid" style="text-decoration: none;">
           <q-btn color="black" icon="map" label="Ir a Google Maps" rounded/>
         </a>
       </div>
-    
+      <Mapbox style="margin-top: 1rem;"/>   
 </template>
 
 
