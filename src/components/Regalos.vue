848337eb5bf32244fac7c688b68a43af650884b0
rubendediego90 <rubendediego90@gmail.com>
2022-12-29 10:51:52 +0100

    regalos revisados

--- src/components/Regalos.vue
+++ src/components/Regalos.vue
@@ -7,10 +7,9 @@
     <div class="col-12 column" style="padding:1rem">
       <p class="text-justify">Vuestra presencia será nuestro mejor regalo. Aún así, si quieres contribuir en nuestra futura vida juntos, os facilitamos el número de cuenta.</p>
       <div class="flex col-12" style="display: flex;justify-content: center;align-items: center">
-        <p class="text-justify" ><strong>ES8801986139751066924632</strong></p>
+        <p class="text-justify" ><strong>ES36 2095 0474 0091 1993 3902</strong></p>
       </div>
       <p class="text-justify" >Tres cosas hay en la vida; salud, dinero y amor. Afortunadamente nos sobran salud y amor.</p>
     </div>
-  <p style="color:red">EL NUMERO DE CUENTA NO ES ESE, CAMBIAR</p>
 
 </template>
