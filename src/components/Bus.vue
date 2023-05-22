fceebb7c47e0be6ef37c518553739c48f03c8b75
rubendediego90 <rubendediego90@gmail.com>
2023-05-22 18:30:45 +0200

    horario bus

--- src/components/Bus.vue
+++ src/components/Bus.vue
@@ -5,8 +5,41 @@
 <template>
   <Title title="Bus"/>
     <div class="col-12 column" style="padding:1rem">
-      <p class="text-justify">Más adelante añadiremos la información del autobus.</p>
+      <ul>
+        <li>
+          <p class="text-justify title-p">Ida a la Vega del Henares</p>
+          <p class="text-justify">Salida desde Madrid. Ministerio de Agricultura (Paseo Santa Isabel Nº1 - Enfrente de Atocha) <u><strong>12:00</strong></u>.</p>
+          <p class="text-justify">Salida desde Alcalá de Henares. Vía Complutense Nº32 (Enfrente de comisiones obreras) a las <u><strong>12:30</strong></u>.</p>
+        </li>
+        <li>
+          <p class="text-justify title-p">Primer regreso desde la finca.</p>
+      <p class="text-justify">Media hora después de que comience el baile (sobre las <u><strong>18:30</strong></u> aprox). Parada en Alcalá (En comisiones obreras) y Madrid (Atocha).</p>
+        </li>
+        <li>
+          <p class="text-justify title-p">Segundo regreso desde la finca.</p>
+          <p class="text-justify">Media hora después de que termine el baile (sobre las <u><strong>22:15</strong></u> aprox). Parada en Alcalá (En comisiones obreras) y Madrid (Atocha).</p>
+        </li>
+      </ul>
     </div>
-    <img style="width:100%;padding:1rem" src="../assets/bus.jpg"/>
+    <img style="width:100%;padding-left:1rem;padding-right:1rem" src="../assets/bus.jpg"/>
 
 </template>
+<style scoped>
+li {
+  margin-bottom: 10px; /* Ajusta el valor según tus necesidades */
+  margin-left: 0px;
+}
+p{
+  margin-bottom: 0px; /* Ajusta el valor según tus necesidades */
+}
+ul{
+  padding-left: 5px;
+  margin-bottom: 0px;
+  padding-bottom: 0px;
+}
+
+.title-p{
+  font-weight: bold;
+  text-decoration: underline;
+}
+</style>
